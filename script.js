9add16cf6b375caa7de6aba59ec1e1ab6dcd880d
dillrofd <148703484+dillrofd@users.noreply.github.com>
2024-11-22 19:59:05 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+console.log( 5+5+"5");
+
+
+
+
+const email = "ynkarrie@gmail.com";
+
+console.log( email.includes ("@"));
+
+console.log (email.length);
+
+
+
+
+const my = "My";
+const name = " Name";
+const is = "is";
+
+let together = `${my}  ${name}  ${is} Viktor `;
+console.log (together);
+
+
+
+
+const userName = "Олександр";
+let payment = "100$";
+alert(`Добрый день ${userName} вы хотите подвердить покупку? К оплате ${payment}`);
